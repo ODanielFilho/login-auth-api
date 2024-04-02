4d3fd44aae34636b417816fd964663f9580ffc8a
Daniel Filho <danielfsf2001@gmail.com>
2024-04-02 17:08:10 -0300

    add user entity

---
+++ src/main/java/com/ODanielFilho/loginauthapi/domain/user/User.java
@@ -0,0 +1,24 @@
+package com.ODanielFilho.loginauthapi.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private  String name;
+    private  String email;
+    private  String password;
+
+}
